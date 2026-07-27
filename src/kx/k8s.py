030a68d38acb6b75230563dc7a67f8855902aa9c
Bartosz Fenski <fenio@debian.org>
2026-07-27 03:27:38 +0200

    fix: use bundled CA for Kubernetes clients (#126)

--- src/kx/k8s.py
+++ src/kx/k8s.py
@@ -1,8 +1,15 @@
-from kubernetes import config
+import certifi
+from kubernetes import client, config
 
 
 def load_config():
+    configuration = client.Configuration()
     try:
-        config.load_kube_config()
-    except Exception:
-        config.load_incluster_config()
+        config.load_kube_config(client_configuration=configuration)
+    except Exception:  # noqa: BLE001
+        config.load_incluster_config(client_configuration=configuration)
+
+    if configuration.verify_ssl and not configuration.ssl_ca_cert:
+        configuration.ssl_ca_cert = certifi.where()
+
+    client.Configuration.set_default(configuration)
